c24ab3f2236e6b24c260f912ac95f04022559852
andreeazbr <andreeazbr@gmail.com>
2026-05-01 22:55:20 +0300

    Am finalizat etapa de preprocesare

--- 02_Preprocessing.py
+++ 02_Preprocessing.py
@@ -7,6 +7,31 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class RareCategoryGrouper(BaseEstimator, TransformerMixin):
+    def __init__(self, threshold=0.01, other_label="Other"):
+        self.threshold = threshold
+        self.other_label = other_label
+        self.frequent_categories_ = {}
+
+    def fit(self, X, y=None):
+        X = pd.DataFrame(X)
+
+        for col in X.columns:
+            freq = X[col].value_counts(normalize=True, dropna=False)
+            self.frequent_categories_[col] = freq[freq >= self.threshold].index.tolist()
+
+        return self
+
+    def transform(self, X):
+        X = pd.DataFrame(X).copy()
+
+        for col in X.columns:
+            frequent_categories = self.frequent_categories_.get(col, [])
+            X[col] = X[col].where(X[col].isin(frequent_categories), self.other_label)
+
+        return X
 
 # Incarcarea setului de date
 adult = fetch_ucirepo(id=2)
@@ -70,3 +95,43 @@ print((y_train.value_counts(normalize=True) * 100).round(2))
 
 print("\nDistribuție clase în test (%):")
 print((y_test.value_counts(normalize=True) * 100).round(2))
+
+numeric_features = X_train.select_dtypes(include=["int64", "float64"]).columns.tolist()
+categorical_features = X_train.select_dtypes(include=["object", "string"]).columns.tolist()
+
+print("\nTrăsături numerice:")
+print(numeric_features)
+
+print("\nTrăsături categoriale:")
+print(categorical_features)
+
+# Pentru variabilele numerice valorile lipsă sunt înlocuite cu mediana calculată pe setul de antrenare
+numeric_transformer = Pipeline(steps=[
+    ("imputer", SimpleImputer(strategy="median"))
+])
+
+# Pentru variabilele categoriale valorile lipsă sunt înlocuite cu cea mai frecventă categorie,
+# se grupeaza categoriile rare, apoi se aplică One-Hot Encoding
+
+categorical_transformer = Pipeline(steps=[
+    ("imputer", SimpleImputer(strategy="most_frequent")),
+    ("rare_grouper", RareCategoryGrouper(threshold=0.01, other_label="Other")),
+    ("encoder", OneHotEncoder(handle_unknown="ignore"))
+])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("num", numeric_transformer, numeric_features),
+        ("cat", categorical_transformer, categorical_features)
+    ]
+)
+
+X_train_processed = preprocessor.fit_transform(X_train)
+X_test_processed = preprocessor.transform(X_test)
+
+print("\nDimensiuni după preprocesare:")
+print("X_train_processed:", X_train_processed.shape)
+print("X_test_processed:", X_test_processed.shape)
+
+print("\nTipul structurii rezultate:")
+print(type(X_train_processed))
