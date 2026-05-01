cb95b089da2c94ea2ed8a78b566b3d4a9d73790d
andreeazbr <andreeazbr@gmail.com>
2026-05-01 21:58:16 +0300

    Am impartit pe Train si Test

--- 02_Preprocessing.py
+++ 02_Preprocessing.py
@@ -30,3 +30,43 @@ print("y:", y.shape)
 print("\nDistribuție target după codificare:")
 print(y.value_counts())
 
+# eliminare duplicate
+
+df_full = pd.concat([X.reset_index(drop=True), y.reset_index(drop=True)], axis=1)
+duplicates_count = df_full.duplicated().sum()
+duplicates_percent = duplicates_count / len(df_full) * 100
+
+print("\nDuplicate înainte de eliminare:")
+print(f"Număr duplicate: {duplicates_count}")
+print(f"Procent duplicate: {duplicates_percent:.2f}%")
+
+df_full = df_full.drop_duplicates().reset_index(drop=True)
+
+X = df_full.drop(columns=["income"])
+y = df_full["income"]
+
+print("\nDimensiune după eliminarea duplicatelor:")
+print("X:", X.shape)
+print("y:", y.shape)
+
+# Impartire Train/Test
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.20,
+    random_state=42,
+    stratify=y
+)
+
+print("\nDimensiuni seturi:")
+print("X_train:", X_train.shape)
+print("X_test:", X_test.shape)
+print("y_train:", y_train.shape)
+print("y_test:", y_test.shape)
+
+print("\nDistribuție clase în train (%):")
+print((y_train.value_counts(normalize=True) * 100).round(2))
+
+print("\nDistribuție clase în test (%):")
+print((y_test.value_counts(normalize=True) * 100).round(2))
